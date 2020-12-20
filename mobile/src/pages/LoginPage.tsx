175af5c09eb7312bf1ce66b1652e63da87003c56
Bruno Mello <brunomello.ti@protonmail.com>
2020-12-20 01:34:23 -0300

    first commit xD

---
+++ mobile/src/pages/LoginPage.tsx
@@ -0,0 +1,115 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import Constants from "expo-constants";
+import { FontAwesome5 } from "@expo/vector-icons";
+
+export default function loginPage() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.viewGuest}>
+        <Text style={styles.textAnimal}>Welcome</Text>
+
+        <TouchableOpacity onPress={() => {}}>
+          <FontAwesome5 name="chevron-right" size={24} />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.viewDescription}>
+        <Text style={styles.textAnimalHelp}>Animal Help</Text>
+        <Text style={styles.description}>
+          Você gosta de animais? Ajude as ONGs e Protetores dos Animais. Estes
+          sim são verdadeiros heróis dos nossos queridos amiguinhos!
+        </Text>
+
+        <TouchableOpacity style={styles.buttonLogin}>
+          <Text style={styles.textButtonLogin}>Log in</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.buttonSignUp}>
+          <Text style={styles.textButtonSignUp}>Cadastrar</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+    marginHorizontal: 24,
+    marginTop: Constants.statusBarHeight + 12,
+  },
+
+  viewGuest: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  textAnimal: {
+    fontFamily: "Nunito_700Bold",
+    fontSize: 24,
+    color: "#00bfa6",
+  },
+
+  viewDescription: {
+    position: "absolute",
+    bottom: 32,
+  },
+
+  textAnimalHelp: {
+    fontFamily: "Nunito_700Bold",
+    fontSize: 32,
+    color: "#00bfa6",
+
+    marginBottom: 24,
+  },
+
+  description: {
+    fontFamily: "Nunito_600SemiBold",
+    fontSize: 18,
+    color: "#3f3d56",
+
+    marginBottom: 24,
+  },
+
+  buttonLogin: {
+    alignItems: "center",
+    justifyContent: "center",
+
+    height: 52,
+    width: "100%",
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: "#3f3d56",
+
+    marginBottom: 12,
+  },
+
+  textButtonLogin: {
+    fontFamily: "Nunito_700Bold",
+    fontSize: 24,
+    color: "#3f3d56",
+  },
+
+  buttonSignUp: {
+    alignItems: "center",
+    justifyContent: "center",
+
+    height: 52,
+    width: "100%",
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: "#00bfa6",
+    backgroundColor: "#00bfa6",
+
+    marginBottom: 12,
+  },
+
+  textButtonSignUp: {
+    fontFamily: "Nunito_700Bold",
+    fontSize: 24,
+    color: "#3f3d56",
+  },
+});
---
+++ mobile/src/routes.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const { Navigator, Screen } = createStackNavigator();
+
+import LoginPage from "./pages/LoginPage";
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Navigator screenOptions={{ headerShown: false }}>
+        <Screen name="LoginPage" component={LoginPage} />
+      </Navigator>
+    </NavigationContainer>
+  );
+}
