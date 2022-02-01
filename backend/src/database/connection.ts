3bf9973e8df9bb92a777ed6ad26efbde04fc43e2
Brunomello-xD <brunomello.ti@protonmail.com>
2022-01-31 21:28:37 -0300

    Safe zone

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -1,5 +1,4 @@
 //Conexão com o banco de dados
-
 import { createConnection } from "typeorm";
 
-createConnection().then(() => console.log("✅ Successful database connection"));
+createConnection().then(() => console.log("✅ Successful database connection"))
--- backend/src/database/migrations/1643080723737-CreateOngs.ts
+++ backend/src/database/migrations/1643080723737-CreateOngs.ts
@@ -43,21 +43,7 @@ export class CreateOngs1643080723737 implements MigrationInterface {
                 type: 'boolean',
                 default: false,
               },
-              {
-                name: 'user_id',
-                type: 'uuid'
-              }
             ],
-            foreignKeys: [
-              {
-                name: 'ongUser',
-                columnNames: ['user_id'],
-                referencedTableName: 'users',
-                referencedColumnNames: ['id'],
-                onUpdate: 'CASCADE',
-                onDelete: 'CASCADE',
-              }
-            ]
           }))  
     }
 
